45ede41f09c37976914be017e55d48df72e4e987
gaurangrshah <gaurang.r.shah@gmail.com>
2020-01-10 03:43:24 -0500

    final update

--- site/gatsby-plugin-theme-ui/index.js
+++ site/gatsby-plugin-theme-ui/index.js
@@ -1,8 +1,8 @@
-import merge from "lodash.merge"
+import merge from 'lodash.merge'
 import { theme } from "gatsby-theme-styleguide"
 
 export default merge({}, theme, {
   colors: {
-    primary: "blue",
-  },
-})
+    primary: 'blue'
+  }
+});
